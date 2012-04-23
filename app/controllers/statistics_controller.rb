c540b800c17f65a5ac9fb732474a12cfca850c3c
Revence Kalibwani <revence@1st.ug>
2012-04-23 11:21:34 +0300

    Added downloading stats for Bio-statistician.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,4 +2,11 @@ class StatisticsController < ApplicationController
   def index
     @subs = CollectedInfo.order('created_at DESC').paginate(:page => request[:page])
   end
+
+  def csv
+    @subs = CollectedInfo.order('created_at DESC').paginate(:page => request[:page])
+    response.headers['Content-Type'] = %[text/csv; encoding=UTF-8]
+    response.headers['Content-Disposition'] = %[attachment; filename=#{Time.now.strftime('%d-%B-%Y-%H%Mh')}.csv]
+    render 'csv.csv.erb'
+  end
 end
