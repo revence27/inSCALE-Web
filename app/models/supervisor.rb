4c8e887b26408223bbe279f6c0836131f877553d
Revence Kalibwani <revence@1st.ug>
2013-09-17 15:03:28 +0300

    Correct link.

--- app/models/supervisor.rb
+++ app/models/supervisor.rb
@@ -4,6 +4,6 @@ class Supervisor < ActiveRecord::Base
   belongs_to  :parish
 
   def goodly_html
-    (self.dormant ? %[<i>#{self.name}</i>] : %[<a href="/supervisors/#{self.id}">#{self.name}</a>]) + %[<sub class="quicksub">#{self.number[3 .. -1]}</sub>]
+    (self.dormant ? %[<i>#{self.name}</i>] : %[<a href="/system/supervisors/#{self.id}">#{self.name}</a>]) + %[<sub class="quicksub">#{self.number[3 .. -1]}</sub>]
   end
 end
